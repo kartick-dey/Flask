20e4e058357debf724f589c0d48246bd4e4d2ec8
Kartick Dey <kartick.dey1995@gmail.com>
2021-02-10 13:56:05 +0530

    shcbdch

--- database_table_creation.py
+++ database_table_creation.py
@@ -7,7 +7,9 @@ def create_table():
     try:
         with connection.cursor() as cursor:
             # create_table_query = "CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(64) NOT NULL, password VARCHAR(255) NOT NULL)"
-            create_table_query = "CREATE TABLE items (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(64) NOT NULL, price INT(25) NOT NULL)"
+            # create_table_query = "CREATE TABLE items (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(64) NOT NULL, price INT(25) NOT NULL)"
+            create_table_query = "CREATE TABLE user (id INT AUTO_INCREMENT PRIMARY KEY, public_id VARCHAR(255) NOT NULL, name VARCHAR(64) NOT NULL, password VARCHAR(255) NOT NULL, admin BOOLEAN NOT NULL)"
+            # create_table_query = "CREATE TABLE todo (id INT AUTO_INCREMENT PRIMARY KEY, text VARCHAR(255) NOT NULL, complete BOOLEAN NOT NULL, user_id INT NOT NULL)"
             cursor.execute(create_table_query)
             connection.commit()
             print('Table created')
@@ -33,6 +35,23 @@ def insert_data():
         connection.close()
         print("Connection closed!")
 
+def delete_table():
+    # DROP TABLE tablename
+    try:
+        with connection.cursor() as cursor:
+            # Create a new record
+            sql = "DROP TABLE user"
+            cursor.execute(sql)
+            # connection.commit()
+            print("Deleted!")
+    except Exception as e:
+        print(e)
+        raise e
+    finally:
+        connection.close()
+        print("Connection closed!")
+
 
 create_table()
 # insert_data()
+# delete_table()
